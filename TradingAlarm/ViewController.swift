645fc854d119ff98b780f371ae8b23aada633a7e
John T Ingato <jtingato@gmail.com>
2023-08-05 09:46:22 -0500

    Initial Commit

---
+++ TradingAlarm/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TradingAlarm
+//
+//  Created by John Ingato on 8/5/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
