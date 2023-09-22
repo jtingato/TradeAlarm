ecc8f5c8cce88b78066060780702f24828765ac9
John T Ingato <jtingato@gmail.com>
2023-09-22 09:12:20 -0500

    WIP - adding SB controllers and VCs for settings page

---
+++ TradingAlarm/Sections/Alert Settings/AlertSettingsViewController.swift
@@ -0,0 +1,16 @@
+//
+//  AlertSettingsViewController.swift
+//  TradingAlarm
+//
+//  Created by John Ingato on 9/22/23.
+//
+
+import UIKit
+
+class AlertSettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
---
+++ TradingAlarm/Sections/Alert Settings/AlertSettingsViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  AlertSettingsViewModel.swift
+//  TradingAlarm
+//
+//  Created by John Ingato on 9/22/23.
+//
+
+import Foundation
+
+struct AlertSettingsViewModel {
+    
+}
